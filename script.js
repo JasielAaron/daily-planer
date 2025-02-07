2c21d90ceb285d3f8dca9856f36005f29a2c1a78
JasielAaron <150881134+JasielAaron@users.noreply.github.com>
2025-02-07 12:03:24 -0700

    motified code to make it dynamic in Javascript

--- script.js
+++ script.js
@@ -1,88 +1,61 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-var localeSettings = {};
-dayjs.locale(localeSettings);
-
 
 $(function () {
-  // Get hour using dayjs libary.
-  var currentHour = dayjs().format("H");
-  //function will change the color of time according to hour, present past future ect.
-  function hourlyColor() {
-    $('.time-block').each(function() {
-      var blockHour = parseInt(this.id);
-      $(this).toggleClass("past", blockHour < currentHour);
-      $(this).toggleClass("present", blockHour === currentHour);
-      $(this).toggleClass("future", blockHour > currentHour);
-      
-    });
+
+  // Displays the current date in the header 
+  $('#currentDay').text(dayjs().format('dddd, MMMM D'));
+
+  // Get Current hour in 24-hour format
+  var currentHour = dayjs().hour();
+
+
+  //Loops to create time blocks for the business hours (9am to 5pm)
+
+  for(let i = 9; i <= 17; i++) {
+    var blockId = 'hour-' + i;
+    var ampm =  i < 12 ? 'AM' : 'PM';
+    var displayHour = i <= 12 ? i : i -12;
+
+    //Create the time block HTML structure Dynamically
+    var timeBlockHtml = `
+    <div id="${blockId}" class="row time-block">
+      <div class="col-2 col-md-1 hour text-center py-3">${displayHour}${ampm}</div>
+      <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+        <i class="fas fa-save" aria-hidden="true"></i>
+      </button>
+    </div>
+  `;
+
+  //Append the new time block to the container 
+  $('.container-lg').append(timeBlockHtml);
   }
 
-  //This function will save whatever the user inputs to localStorage when the save button is clicked
-  function textEntry() {
-    $(".saveBtn").on("click", function(){
-      var key = $(this).parent().attr("id");
-      var value = $(this).siblings(".description").val();
-      localStorage.setItem(key,value);
-    });
-  }
+  // Loop through each time block and apply past, present, or future class
+  $('.time-block').each(function () {
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
-  // will refresh the color of each time block
-  function refreshColor() {
-    $(".time-block").each(function(){
-      var blockHour = parseInt(this.id);
-      if (blockHour == currentHour) {
-        $(this).removeClass("past future").addClass("present");
-      }else if (blockHour < currentHour) {
-        $(this).removeClass("future present").addClass("past");
-      }else {
-        $(this).removeClass("past present").addClass("future");
-      }
-    });
-  }
-// Gets user input from the localStorage also set textarea for each time block 
-$(".time-block").each(function(){
-  var key = $(this).attr("id");
-  var value = localStorage.getItem(key);
-  $(this).children(".description").val(value);
+    //Add class based on the comparision between the current hour and the block hour 
+
+    if(blockHour < currentHour) {
+      $(this).addClass('past');
+    } else if (blockHour === currentHour) {
+      $(this).addClass('present'); 
+    } else {
+      $(this).addClass('future');
+    }
+
+    var savedTask = localStorage.getItem(blockHour);
+    if (savedTask) {
+      $(this).find('.description').val(savedTask);
+    }
+  });
+
+  //Add click Event lister to save buttons 
+
+  $('.saveBtn').on('click', function (){
+    var hour = $(this).closest('.time-block').attr('id').split('-')[1];
+    var task = $(this).siblings('.description').val();
+    //Saves the task in local storage
+localStorage.setItem(hour, task);
+  });
 });
-//to update date and time 
-function currentTime() {
-  var dateEl = $("#date");
-  var timeEl = $("#time");
-  var currentDay = dayjs().format(" dddd MMMM DD, YYYY");
-  var currentTime = dayjs().format("hh:mm:ss A ");
-  dateEl.text(currentDay);
-  timeEl.text(currentTime);
-} 
-// call functions to set up the page 
-hourlyColor();
-textEntry();
-refreshColor();
-//will update time 
-setInterval(currentTime, 1000);
-
-});
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
